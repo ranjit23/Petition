c8c9395167b7ca7ffbcef628a0de96a417de7d35
Bhaven Kakade <thisisbhaven@gmail.com>
2018-10-03 16:14:18 +0530

    index create

--- app/Http/Controllers/PetitionController.php
+++ app/Http/Controllers/PetitionController.php
@@ -14,7 +14,7 @@ class PetitionController extends Controller
      */
     public function index()
     {
-        //
+        return view('petition.index');
     }
 
     /**
@@ -24,7 +24,7 @@ class PetitionController extends Controller
      */
     public function create()
     {
-        //
+        return view('petition.create');
     }
 
     /**
